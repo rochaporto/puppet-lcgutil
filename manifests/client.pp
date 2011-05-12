90037ec618283df327d72e78f85f3818a29946c4
root <root@vmdm0006.cern.ch>
2011-05-12 16:42:35 +0200

    Renamed module glite to new name grid-common.

--- manifests/client.pp
+++ manifests/client.pp
@@ -10,5 +10,15 @@
 # CERN IT/GT/DMS <it-dep-gt-dms@cern.ch>
 #
 class lcgutil::client {
-    package { "lcg_util": ensure => latest, notify => Exec["glite_ldconfig"], }
+  package { "lcg-util": 
+    name   => $grid_flavour ? { 
+      "glite" => "lcg_util",
+      default => $name,
+    },
+    ensure => latest, 
+    notify => $grid_flavour ? {
+      "glite" => Exec["glite_ldconfig"],
+      default => undef,
+    }
+  }
 }
